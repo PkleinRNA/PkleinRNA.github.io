fb74ce88ba92fec782dfec7286cf876372d9fefc
PkleinRNA <klein.pierre1@gmail.com>
2024-07-27 17:19:11 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,7 +14,6 @@
         }
     </style>
     <script>
-        // Determine the expected state of the theme toggle, which can be "dark", "light", or "system". Default is "system".
         let determineThemeSetting = () => {
             let themeSetting = localStorage.getItem("theme");
             if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
@@ -23,7 +22,6 @@
             return themeSetting;
         };
 
-        // Determine the computed theme, which can be "dark" or "light". If the theme setting is "system", the computed theme is determined based on the user's system preference.
         let determineComputedTheme = () => {
             let themeSetting = determineThemeSetting();
             if (themeSetting == "system") {
@@ -38,28 +36,24 @@
             }
         };
 
-        // Apply the initial theme setting.
         let applyInitialTheme = () => {
             let theme = determineComputedTheme();
             document.documentElement.setAttribute("data-theme", theme);
             if (theme === "dark") {
                 document.body.classList.add('dark-theme');
+            } else {
+                document.body.classList.remove('dark-theme');
             }
         };
 
-        // Apply the initial theme as early as possible to prevent flickering
         applyInitialTheme();
     </script>
 </head>
 <body>
-    <!-- This button will trigger the theme toggle -->
     <button id="light_toggle">Toggle Theme</button>
-
-    <!-- This hidden button is the actual theme toggle button -->
     <button id="theme_toggle" style="display:none;">Hidden Toggle</button>
 
     <script>
-        // Make sure the DOM is fully loaded before attaching the event listener
         document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('light_toggle').addEventListener('click', function() {
                 document.getElementById('theme_toggle').click();
@@ -71,16 +65,11 @@
         });
 
         function switchTheme() {
-            // Get the body element
             var body = document.body;
-
-            // Check if the body has the class 'dark-theme'
             if (body.classList.contains('dark-theme')) {
-                // If it does, remove it
                 body.classList.remove('dark-theme');
                 localStorage.setItem('theme', 'light');
             } else {
-                // If it doesn't, add it
                 body.classList.add('dark-theme');
                 localStorage.setItem('theme', 'dark');
             }
