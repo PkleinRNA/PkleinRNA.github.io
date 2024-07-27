dffd86b84fded0f2fe6e91fffbe49a461d69479d
PkleinRNA <klein.pierre1@gmail.com>
2024-07-27 17:09:56 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -364,3 +364,47 @@ document.addEventListener('DOMContentLoaded', function () {
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
   updateTheme(prefersDarkScheme);
 });
+
+
+// Global variable to keep track of the current theme
+let isDarkMode = false;
+
+// Common function to toggle theme
+function toggleTheme() {
+  isDarkMode = !isDarkMode;
+  updateTheme();
+}
+
+// Update theme function
+function updateTheme() {
+  if (isDarkMode) {
+    document.body.classList.add('dark-mode');
+    lightToggleIcon.classList.remove('ti-sun-filled');
+    lightToggleIcon.classList.add('ti-moon-filled');
+    themeToggleLight.style.display = 'none';
+    themeToggleDark.style.display = 'inline';
+  } else {
+    document.body.classList.remove('dark-mode');
+    lightToggleIcon.classList.remove('ti-moon-filled');
+    lightToggleIcon.classList.add('ti-sun-filled');
+    themeToggleLight.style.display = 'inline';
+    themeToggleDark.style.display = 'none';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lightToggle = document.getElementById('light-toggle');
+  const themeToggle = document.getElementById('theme-toggle');
+  const lightToggleIcon = document.getElementById('light-toggle-icon');
+  const themeToggleLight = document.getElementById('theme-toggle-light');
+  const themeToggleDark = document.getElementById('theme-toggle-dark');
+
+  // Attach the common function to both buttons
+  lightToggle.addEventListener('click', toggleTheme);
+  themeToggle.addEventListener('click', toggleTheme);
+
+  // Optionally initialize the theme based on a saved preference or system setting
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  isDarkMode = prefersDarkScheme;
+  updateTheme();
+});
