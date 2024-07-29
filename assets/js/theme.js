f73f979fc11159461509f1564e8623f0339e605f
PkleinRNA <klein.pierre1@gmail.com>
2024-07-29 16:13:26 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -25,7 +25,7 @@ let setThemeSetting = (themeSetting) => {
 let applyTheme = () => {
   let theme = determineComputedTheme();
 
-
+  transTheme(theme);
   setHighlight(theme);
   setGiscusTheme(theme);
   setSearchTheme(theme);
